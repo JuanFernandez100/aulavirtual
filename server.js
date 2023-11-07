10014a5a99fbdb6a638542cb8c842a17429a3e2e
Moisés Gamero <moises1109@gmail.com>
2023-11-07 13:02:37 -0500

    moy2

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
+//const strapi = require('strapi');
+//strapi().start();
+
 const strapi = require('strapi');
-strapi().start();
+
+(async () => {
+  // Crear una instancia de Strapi
+  const strapiApp = await strapi();
+
+  // Configurar autoReload para desarrollo
+  if (process.env.NODE_ENV === "development") {
+    strapiApp.autoReload = { enabled: true };
+  }
+
+  // Cargar y arrancar Strapi
+  await strapiApp.load();
+  await strapiApp.start();
+})();
